5f37fcf296298f8fceeca616251e5b7f7f1be93c
Samuel Ramos <samuelsantos7@yahoo.com.br>
2020-07-09 02:51:49 -0300

    Configurações Iniciais do Projeto

---
+++ server/src/app.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import './database';
+
+import routes from './routes';
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+export default app;
---
+++ server/src/server.ts
@@ -0,0 +1,6 @@
+import 'reflect-metadata';
+import app from './app';
+
+app.listen(3333, () => {
+  console.log('✔ Server started on port 3333!');
+});
